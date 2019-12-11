5953d1108f4d37878761301db5a0c24bf3dc4197
Ignacio M. Tirabasso <ignacio.tirabasso@gmail.com>
2019-12-11 01:57:03 -0300

    using buidler-erasure on testenv

---
+++ buidler.config.js
@@ -0,0 +1,46 @@
+usePlugin("../node_modules/buidler-erasure");
+
+task("compile", async () => {
+  console.log('no compile')
+});
+
+const customSetup = require('./custom.config')
+// deploySetup = require("./src/deploy.config");
+// localSetup = require("./src/local.config");
+// mainnetSetup = require("./src/mainnet.config");
+// rinkebySetup = require("./src/rinkeby.config");
+
+module.exports = {
+  paths: {
+    cache: 'packages/testenv/cache',
+    artifacts: 'packages/testenv/build',
+    sources: './contracts'
+  },
+  solc: {
+    evmVersion: "constantinople",
+    version: "0.5.13"
+  },
+  networks: {
+    // buidlerevm: {
+      // erasureSetup: customSetup
+    // },
+    develop: {
+      url: "http://127.0.0.1:8545",
+      accounts: {
+        mnemonic:
+          "myth like bonus scare over problem client lizard pioneer submit female collect",
+        path: "m/44'/60'/0'/0/",
+        initialIndex: 0,
+        count: 20
+      }
+    },
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/59d7e2fc73234fb89300603dc531c877",
+      accounts: [
+      ]
+    },
+    mainnet: {
+      url: "http://127.0.0.1:8545"
+    }
+  }
+};
---
+++ script.js
@@ -0,0 +1,85 @@
+// const ethers = require('ethers')
+const assert = require('assert')
+const ganache = require('ganache-cli')
+const {
+  hexlify,
+  createIPFShash,
+  abiEncodeWithSelector,
+} = require('./packages/testenv/utils')
+
+const env = require('@nomiclabs/buidler')
+const { ethers, utils } = require('ethers')
+
+const main = async () => {
+  console.log(``)
+  console.log(`Create test instance from factories`)
+  console.log(``)
+
+  const userAddress = '0x6087555A70E2F96B7838806e7743041E035a37e5'
+  const proofhash = ethers.utils.sha256(ethers.utils.toUtf8Bytes('proofhash'))
+  const IPFShash = createIPFShash('multihash')
+  console.log(`userAddress: ${userAddress}`)
+  console.log(`proofhash: ${proofhash}`)
+  console.log(`IPFShash: ${IPFShash}`)
+  console.log(``)
+
+  await env.run('erasure:erasure-setup')
+
+  const nmr = await env.erasure.getContractInstance('NMR')
+
+  // Feed
+  const feed = await env.erasure.createInstance('Feed', [
+    userAddress,
+    proofhash,
+    IPFShash,
+  ])
+
+  // // SimpleGriefing
+  const griefing = await env.erasure.createInstance('SimpleGriefing', [
+    userAddress,
+    userAddress,
+    userAddress,
+    ethers.utils.parseEther('1'),
+    2,
+    IPFShash,
+  ])
+
+  // // CountdownGriefing
+  const countdownGriefing = await env.erasure.createInstance(
+    'CountdownGriefing',
+    [
+      userAddress,
+      userAddress,
+      userAddress,
+      ethers.utils.parseEther('1'),
+      2,
+      100000000,
+      IPFShash,
+    ],
+  )
+
+  const abiEncoder = new ethers.utils.AbiCoder()
+  // CountdownGriefingEscrow
+  await env.erasure.createInstance(
+    'CountdownGriefingEscrow',
+    [
+      userAddress,
+      userAddress,
+      userAddress,
+      ethers.utils.parseEther('1'),
+      ethers.utils.parseEther('1'),
+      100000000,
+      IPFShash,
+      abiEncoder.encode(
+        ['uint256', 'uint8', 'uint256'],
+        [ethers.utils.parseEther('1'), 2, 100000000],
+      ),
+    ],
+  )
+
+  return 0;
+}
+
+main()
+  .then(v => console.log('done', v))
+  .catch(err => console.error(err))
